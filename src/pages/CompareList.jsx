cbdffb2fa35960e0608bc6729f61132283d0a7db
Erremme <Erremme2.0@gmail.com>
2025-06-18 10:17:40 +0200

    Feat : make style to compareList

--- src/pages/CompareList.jsx
+++ src/pages/CompareList.jsx
@@ -5,22 +5,44 @@ export default function CompareList() {
   const { compareList, removeFromCompare } = useCompare();
 
   return (
-    <div className="compare-list-container">
-      <h1>Confronta le tue bici</h1>
+    <div  className="compare-list-page">
+     <h1>Confronta le tue bici</h1>
       {compareList.length === 0 ? (
         <p>Nessuna bici da confrontare.</p>
-      ) : (
+      ): (
+      <div className="compare-list-container">
+      
         <ul className="compare-list">
           {compareList.map((bike) => (
             <li key={bike.id} className="compare-item">
               <img src={bike.image} alt={bike.title} />
               <h2>{bike.title}</h2>
-              <p>{bike.description}</p>
+              <p><strong>Cambio:</strong>  {bike.gear}</p>
+              <p><strong>Motore :</strong> {bike.engine}</p>
+              <p><strong>Peso :</strong> {bike.weight} Kg</p>
+              <p><strong>Capacità di peso :</strong> {bike.weight_capacity} Kg</p>
+              <p><strong>Prezzo :</strong> {bike.price},00€ </p>
+              <h3>Forma del telaio :</h3>
+              {bike.frame_variants && 
+                     bike.frame_variants.map((variant,index) => {
+                        return (
+                          <div key={index}>
+                            
+                            <img src={variant.frame_image} alt={variant.name} />
+                            <h4>{variant.name}</h4>
+                            
+                          </div>
+                        );
+                     })           
+              }
+
               <button onClick={() => removeFromCompare(bike)}>Rimuovi</button>
             </li>
           ))}
         </ul>
-      )}
+      
+    </div>
+    )}
     </div>
   );
 }
