3f6fd18fc5fb7ca36c1df7b0d8f2e6d82246ac59
Erremme <Erremme2.0@gmail.com>
2025-06-23 15:48:45 +0200

    Feat : added folder database and file types

---
+++ types.ts
@@ -0,0 +1,16 @@
+export type Ebike = {
+    title : string,
+    category : string,
+    image : string,
+    price: number,
+    description : string,
+    gear : string,
+    engine : string,
+    weight_capacity: number,
+    weight : number,
+    frame_variants: object[]
+
+}
+
+
+
